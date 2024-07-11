4699742ad151f9c86a68e5199be49af446abdfa5
duran550 <138498476+duran550@users.noreply.github.com>
2024-07-11 21:46:35 +0100

    fix: did minor fixes

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
-import Animations from "../../utilities/Animations";
+// import Animations from "../../utilities/Animations";
 import "./AboutMe.css";
 
 export default function AboutMe(props) {
