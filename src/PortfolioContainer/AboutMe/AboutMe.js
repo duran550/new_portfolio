ebcfb1236e572b38bdc5b31f28cd72cd7c1d54db
duran550 <138498476+duran550@users.noreply.github.com>
2024-07-11 22:01:36 +0100

    fix: did minor fixes

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
-// import Animations from "../../utilities/Animations";
+import Animations from "../../utilities/Animations";
 import "./AboutMe.css";
 
 export default function AboutMe(props) {
-  // let fadeInScreenHandler = (screen) => {
-  //   if (screen.fadeInScreen !== props.id) return;
-  //   Animations.animations.fadeInScreen(props.id);
-  // };
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
 
-  // const fadeInSubscription =
-    // ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
   const SCREEN_CONSTANTS = {
     description:
